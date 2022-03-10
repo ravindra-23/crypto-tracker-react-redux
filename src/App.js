54736ebe2b0d5441bfab517a90f5b839c05b64e3
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-10 22:28:15 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { Navbar } from './components'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home, Cryptocurrencies, News } from './pages';
+import { CssBaseline } from '@material-ui/core';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
+      <CssBaseline />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/cryptocurrencies'>
+          <Cryptocurrencies />
+        </Route>
+        <Route exact path='/news'>
+          <News />
+        </Route>
+      </Switch>
     </Router>
   )
 }
