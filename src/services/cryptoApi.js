26abca0759b6832fd5f20feb1af07f7bd8e743fa
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-10 22:30:46 +0530

    added cryptoApi to fetch data

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '59c45adaa2msh894e55ae53dc4dap181417jsn5e2284951063'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+      getCryptos: builder.query({
+        query: (count) => createRequest(`/coins`),
+      }),
+    })
+})
+
+export const { useGetCryptosQuery } = cryptoApi
