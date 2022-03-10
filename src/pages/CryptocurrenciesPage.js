361fece91a5ae012affaddceb1d8f071bf6dd1f8
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-11 00:58:26 +0530

    added cryptocurrencies page

---
+++ src/pages/CryptocurrenciesPage.js
@@ -0,0 +1,14 @@
+import { CssBaseline } from '@material-ui/core'
+import React from 'react'
+import { Cryptocurrencies } from '../components'
+
+const CryptocurrenciesPage = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Cryptocurrencies />
+    </>
+  )
+}
+
+export default CryptocurrenciesPage
