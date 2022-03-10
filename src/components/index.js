c0143c66bcf46a159309c5e7041aa58b9a3f4a05
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-11 00:56:06 +0530

    imports

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar/Navbar'
+export { default as HomeInfo } from './Home-Info/HomeInfo'
+export { default as Cryptocurrencies } from './Cryptocurrencies/Cryptocurrencies'
+export { default as News } from './News/News'
