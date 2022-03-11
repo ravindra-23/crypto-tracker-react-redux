38468e3f013d5d62f2841a2d9ffa5612c40760d2
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-11 18:46:15 +0530

    added cryptonewsapi to fetch news

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '59c45adaa2msh894e55ae53dc4dap181417jsn5e2284951063'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+      getCryptoNews: builder.query({
+        query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+      }),
+    })
+})
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
